6501ccbb09649b2e02ef016be1b9bed85a71d2de
fatima-ahmed <fatima941995@gmail.com>
2021-08-02 17:47:10 +0200

    [ADD]header custom module

---
+++ cexcellent_custom_header/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "C Excellent Custom Header",
+
+    'summary': """extend external layout""",
+
+    'description': """
+        Report header
+    """,
+
+    'author': "c-excellent",
+    'website': "http://www.yourcompany.com",
+    'category': 'Hidden',
+    'version': '0.1',
+    'depends': ['web'],
+    'data': [
+        'views/extend_layout_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
