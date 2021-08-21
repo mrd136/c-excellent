023b7fb106101feef83570ef13f5bf0329bdde97
fatima-ahmed <fatima941995@gmail.com>
2021-08-21 17:11:42 +0200

    [ADD]whatsapp

--- ce_referral_hms/models/operating_unit.py
+++ ce_referral_hms/models/operating_unit.py
@@ -5,7 +5,7 @@ class OperatingUnit(models.Model):
     _inherit = "operating.unit"
     _description = "Operating Unit"
 
-    referral_user_ids = fields.Many2many("res.users", "referral_users_rel", "operating_unit_id", "user_id",
-                                         "Users responsible for receive referral")
+    # referral_user_ids = fields.Many2many("res.users", "referral_users_rel", "operating_unit_id", "user_id",
+    #                                      "Users responsible for receive referral")
     referral_coordinator_ids = fields.Many2many("res.users", "referral_coordinator_rel", "operating_unit_id", "user_id",
                                                 "Users Coordinator referral")
